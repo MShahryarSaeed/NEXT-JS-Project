f4b3e9263badf994d120bd583fa045f79af6a451
MShahryarSaeed <shahryarsaeed06@gmail.com>
2024-05-10 06:51:08 +0500

    complete HeaderTop

--- src/app/components/HeaderTop.js
+++ src/app/components/HeaderTop.js
@@ -3,35 +3,36 @@ import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 const HeaderTop = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div>
+    <div className="border-b border-gray-200 hidden sm:block">
+      <div className="container py-4">
+        <div className="flex justify-between items-center mx-auto">
+          <div className="hidden lg:flex gap-1">
+            <div className="header-top-icon-wrapper">
               <BsFacebook />
             </div>
-            <div>
+            <div className="header-top-icon-wrapper">
               <BsTwitter />
             </div>
-            <div>
+            <div className="header-top-icon-wrapper">
               <BsInstagram />
             </div>
-            <div>
+            <div className="header-top-icon-wrapper">
               <BsLinkedin />
             </div>
           </div>
           <div className="text-gray-500 text-[12px]">
-            <strong>FREE SHIPPING</strong>
+            <strong>FREE SHIPPING </strong>
             THIS WEEK ORDER OVER -$55
           </div>
-          <div>
-            <select name="currency" id="currency">
+          <div className="flex gap-4"> 
+            <select name="currency" id="currency" className="text-gray-500 text-[12px] w-[70px]">
               <option value="usd">USD $</option>
               <option value="eur">EUR $</option>
               <option value="rs">PKR Rs</option>
             </select>
 
-            <select name="langugae" id="language">
+            <select name="langugae" id="language"
+            className="text-gray-500 text-[12px] w-[70px]">
               <option value="eng">ENG</option>
               <option value="urdu">URDU</option>
               <option value="hindi">HINDI</option>
