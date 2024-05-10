e1d98c35358d65435f381bfa6f8c1c491ade11da
MShahryarSaeed <shahryarsaeed06@gmail.com>
2024-05-10 06:32:20 +0500

    Add Content to the TopHeader

--- src/app/components/HeaderTop.js
+++ src/app/components/HeaderTop.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 const HeaderTop = () => {
   return (
     <div>
       <div>
         <div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <div>
+              <BsFacebook />
+            </div>
+            <div>
+              <BsTwitter />
+            </div>
+            <div>
+              <BsInstagram />
+            </div>
+            <div>
+              <BsLinkedin />
+            </div>
+          </div>
+          <div className="text-gray-500 text-[12px]">
+            <strong>FREE SHIPPING</strong>
+            THIS WEEK ORDER OVER -$55
+          </div>
+          <div>
+            <select name="currency" id="currency">
+              <option value="usd">USD $</option>
+              <option value="eur">EUR $</option>
+              <option value="rs">PKR Rs</option>
+            </select>
+
+            <select name="langugae" id="language">
+              <option value="eng">ENG</option>
+              <option value="urdu">URDU</option>
+              <option value="hindi">HINDI</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
